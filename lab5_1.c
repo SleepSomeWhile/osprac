07b26d85140b5c6b0884235e5b0146323f047b8d
Andrew Rizo <71076007+SleepSomeWhile@users.noreply.github.com>
2021-02-15 20:16:00 +0300

    Lab 5 done.
    Ризо Андрей БПИ194

---
+++ lab5_1.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+int main()
+{
+    int fd;
+    size_t size;
+    char string[14];
+
+    (void)umask(0);
+
+    if ((fd = open("myfile", O_RDONLY)) < 0) {
+        printf("Can\'t open file\n");
+        exit(-1);
+    }
+
+
+    size = read(fd, string, 14);
+    
+    if (size != 14) {
+        printf("Can\'t read all string\n");
+        exit(-1);
+    }
+
+    printf("%s\n", string);
+
+    if (close(fd) < 0)
+        printf("Can\'t close file\n");
+    return 0;
+}
